e5a16e69199189d221a940ece45771d946cbbd59
Ferdy Daniel Iglesias <ferdyiglesias12@gmail.com>
2022-03-08 09:38:41 -0600

    primera aplicacion sencilla con node js

---
+++ app.js
@@ -0,0 +1,3 @@
+const { frutas, precios } = require('./frutas')
+
+frutas.forEach((fruta) => console.log(fruta, precios))
---
+++ frutas.js
@@ -0,0 +1,7 @@
+const frutas = ['🍓', '🍌', '🍎', '🍉']
+const precios = [100, 200, 300, 400]
+
+module.exports = {
+  frutas: frutas,
+  precios,
+}
